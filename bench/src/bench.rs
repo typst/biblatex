9c524cecc1129082071102ac65e9e76bc6599979
Laurenz <laurmaedje@gmail.com>
2020-12-02 01:01:29 +0100

    Disable default features for regex dependency 🔻

--- bench/src/bench.rs
+++ bench/src/bench.rs
@@ -3,6 +3,7 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use biblatex::Bibliography;
 
 const GRAL: &str = include_str!("../../tests/gral.bib");
+const CROSS: &str = include_str!("../../tests/cross.bib");
 
 fn benchmarks(c: &mut Criterion) {
     macro_rules! bench {
@@ -17,6 +18,10 @@ fn benchmarks(c: &mut Criterion) {
     bench!("parse-gral": Bibliography::parse(GRAL));
     bench!("get-last-gral": bib.get("adedeji2017"));
     bench!("get-author-gral": entry.author());
+
+    let bib = Bibliography::parse(CROSS);
+    let entry = bib.get("issue201").unwrap();
+    bench!("get-date-cross": entry.date());
 }
 
 criterion_group!(benches, benchmarks);
